55f0f38b8b6fe6570e8fced058b94eadf8ab549f
Sam Wagner <60307610+ideadesignmedia@users.noreply.github.com>
2022-11-21 13:26:56 -0700

    Update compile-casino.sol

--- contracts/compile-casino.sol
+++ contracts/compile-casino.sol
@@ -10,17 +10,14 @@ contract Casino {
     ChipInterface public chipContract;
 
     address payable owner;
-
-    uint256 public membershipFee = 1 * 10**16;
-    uint48 public initialChipCount = 1000;
+    uint256 public chipPrice = .001 ether;
+    uint256 public membershipFee = .015 ether;
+    uint48 public initialChipCount = 500;
 
     event ChipsGiven(address indexed user, uint48 amount, uint48 timestamp);
+    event ChipsPurchased(address indexed user, uint48 amount, uint48 timestamp);
     event ChipsTaken(address indexed user, uint48 amount, uint48 timestamp);
-    event NewMember(
-        address indexed player,
-        uint48 timestamp,
-        uint48 initialChipCount
-    );
+    event NewMember(address indexed player, uint48 timestamp, uint48 initialChipCount);
     address[] memberAddresses;
     mapping(address => bool) public members;
     mapping(address => bool) games;
@@ -28,16 +25,6 @@ contract Casino {
     constructor(address chips) {
         chipContract = ChipInterface(chips);
         owner = payable(msg.sender);
-    }
-
-    modifier noReentry {
-        require(!members[msg.sender], "You are already a member of the casino.");
-        _;
-    }
-
-    modifier onlyMembers() {
-        require(members[msg.sender], "Only members can use this function.");
-        _;
     }
 
     modifier onlyOwner() {
@@ -50,12 +37,12 @@ contract Casino {
         _;
     }
 
-    function transferOwnership(address payable newOwner) external onlyOwner {
+    function transferOwnership(address payable newOwner) public onlyOwner {
         owner = newOwner;
     }
 
     function setChipContract(address newContract, bool supplyMembers)
-        external
+        public
         onlyOwner
     {
         chipContract = ChipInterface(newContract);
@@ -66,15 +53,20 @@ contract Casino {
         }
     }
 
-    function setInitialChipCount(uint48 newChipCount) external onlyOwner {
+    function setInitialChipCount(uint48 newChipCount) public onlyOwner {
         initialChipCount = newChipCount;
     }
 
-    function setMembershipFee(uint256 fee) external onlyOwner {
+    function setMembershipFee(uint256 fee) public onlyOwner {
         membershipFee = fee;
     }
 
-    function joinCasino() external payable noReentry {
+    function setChipPrice(uint256 price) public onlyOwner {
+        chipPrice = price;
+    }
+
+    function joinCasino() public payable {
+        require(!members[msg.sender], "You are already a member of the casino.");
         require(msg.value == membershipFee, "Must send membershipFee");
         owner.transfer(msg.value);
         memberAddresses.push(msg.sender);
@@ -82,22 +74,31 @@ contract Casino {
         chipContract.mint(msg.sender, initialChipCount);
         emit NewMember(msg.sender, uint48(block.timestamp), initialChipCount);
     }
+    
+    function buyChips(uint48 amount) public payable {
+        require(members[msg.sender], "You must be a member to buy chips.");
+        require(msg.value == amount * chipPrice, "Must send correct amount.");
+        owner.transfer(msg.value);
+        emit ChipsPurchased(msg.sender, amount, uint48(block.timestamp));
+        chipContract.mint(msg.sender, amount);
+        emit ChipsGiven(msg.sender, amount, uint48(block.timestamp));
+    }
 
-    function giveChips(address to, uint48 amount) external onlyGames {
+    function giveChips(address to, uint48 amount) public onlyGames {
         chipContract.mint(to, amount);
         emit ChipsGiven(to, amount, uint48(block.timestamp));
     }
 
-    function takeChips(address from, uint48 amount) external onlyGames {
+    function takeChips(address from, uint48 amount) public onlyGames {
         chipContract.burn(from, amount);
         emit ChipsTaken(from, amount, uint48(block.timestamp));
     }
 
-    function addGame(address game) external onlyOwner {
+    function addGame(address game) public onlyOwner {
         games[game] = true;
     }
 
-    function removeGame(address game) external onlyOwner {
+    function removeGame(address game) public onlyOwner {
         games[game] = false;
     }
 
